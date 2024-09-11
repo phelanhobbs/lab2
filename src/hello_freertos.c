f0f0334e5c7d90001cc34a904873ced549c68acd
JTAG <jtag@debian12.housing.utah.edu>
2024-09-11 12:56:41 -0600

    Added comments

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -16,28 +16,48 @@ bool led_is_on;
 
 void blink_task(__unused void *params)
 {
+    // Initialize global vars
     counter = 0;
     led_is_on = false;
+
+    // Initialize wireless hardware and check okay
     hard_assert(cyw43_arch_init() == PICO_OK);
+
+    // Set the GPIO LED pin low
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
+
     while (true) {
-	cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_is_on);
-	if (counter++ % 47)
-	    led_is_on = !led_is_on;
-        vTaskDelay(1000);
+        // Update the GPIO LED pin to current LED state
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, led_is_on);
+        
+        // Do not toggle every 47th pulse
+        if (counter++ % 47)
+            // Toggle LED state
+            led_is_on = !led_is_on;
+
+            // Delay 1 second
+            vTaskDelay(1000);
     }
 }
 
 
 void main_task(__unused void *params)
 {
+    // Create a task called BlinkThread
     xTaskCreate(blink_task, "BlinkThread", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+    
+    // Initialize local vars
     int count = 0;
     char c;
+    
+    // Loop through all characters until getchar is false
     while(c = getchar()) {
-	if (c <= 'z' && c >= 'a') putchar(c - 32);
-	else if (c >= 'A' && c <= 'Z') putchar(c + 32);
-	else putchar(c);
+	    if (c <= 'z' && c >= 'a') // Char is lowercase
+            putchar(c - 32); // Set char uppercase
+	    else if (c >= 'A' && c <= 'Z') // Char is uppercase
+            putchar(c + 32); // Set char lowercase
+	    else 
+            putchar(c); // Do not modify char
     }
 }
 
@@ -45,7 +65,11 @@ int main( void )
 {
     stdio_init_all();
     TaskHandle_t task;
+
+    // Create a task called mainThread
     xTaskCreate(main_task, "MainThread", MAIN_TASK_STACK_SIZE, NULL, MAIN_TASK_PRIORITY, &task);
+    
+    // Start the task scheduler
     vTaskStartScheduler();
     return 0;
 }
