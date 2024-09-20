8f13788f183c848cb8b05c9409f42dd3856d92c0
JTAG <jtag@debian12.housing.utah.edu>
2024-09-19 20:02:37 -0600

    Fixed Function header to compile

--- include/functions.h
+++ include/functions.h
@@ -1,13 +1,5 @@
-
-
 #ifndef _FUNCTIONS_H_
 #define _FUNCTIONS_H_
-
-#include "FreeRTOS.h"
-
-#include "pico/stdlib.h"
-#include "pico/multicore.h"
-#include "pico/cyw43_arch.h"
 
 void blink_update(int *counter, bool *state);
 
--- src/functions.c
+++ src/functions.c
@@ -1,4 +1,20 @@
+#include "FreeRTOS.h"
+
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/cyw43_arch.h"
+
 #include "functions.h"
+
+void invert_case(char in, char *out)
+{
+    if (in <= 'z' && in >= 'a') // Char is lowercase
+        *out = (in - 32); // Set char uppercase
+    else if (in >= 'A' && in <= 'Z') // Char is uppercase
+        *out = (in + 32); // Set char lowercase
+    else 
+        *out = in; // Do not modify char
+}
 
 void blink_update(int *counter, bool *state)
 {
@@ -11,15 +27,4 @@ void blink_update(int *counter, bool *state)
 
     // Delay 0.5 second
     vTaskDelay(500);
-}
-
-
-void invert_case(char in, char *out)
-{
-    if (in <= 'z' && in >= 'a') // Char is lowercase
-        *out = (in - 32); // Set char uppercase
-    else if (in >= 'A' && in <= 'Z') // Char is uppercase
-        *out = (in + 32); // Set char lowercase
-    else 
-        *out = in; // Do not modify char
 }
